ec3cb064b3f2564cf58af8d8626dd78d51d17d97
Jivko Chobanov <zhizho91@gmail.com>
2013-01-03 14:38:28 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+art-pazar
+=========
+
+Art Market is an e-commerce project for hand-made goods
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtPazar::Application.config.secret_token = 'b6b89b08357804b8456febdf91cd1603ceafd252a859592bb3be1c1d8db71b05cbc551eb3e12e74b85ccbd120b7432bd88ca6798a1a79a8d40ac419c8ebe452b'
